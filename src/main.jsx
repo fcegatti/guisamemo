73ef9d2de0ab8257aa04f0f18330a55daf0909fb
fcegatti <fcegatti@yahoo.es>
2025-04-01 04:45:29 +0200

    feat: register service worker on page load

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,16 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('✅ Service Worker registrado con éxito:', registration)
+      })
+      .catch((error) => {
+        console.error('❌ Error al registrar el Service Worker:', error)
+      })
+  })
+}

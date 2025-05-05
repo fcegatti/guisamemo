fe3b55bd5d6e5892a1d9c9812b78880f3402e939
fcegatti <fcegatti@yahoo.es>
2025-05-05 00:33:41 -0300

    feat: apply data-theme attribute dynamically in main.jsx for dark mode support

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRouter from './AppRouter.jsx'
 import { LanguageProvider } from '@context/LanguageContext'
-import { ThemeProvider } from '@context/ThemeContext'
+import { ThemeProvider, useTheme } from '@context/ThemeContext'
 import '@styles/index.css'
+
+function RootWrapper () {
+  const { theme } = useTheme()
+
+  useEffect(() => {
+    document.body.classList.toggle('theme--dark', theme === 'dark')
+  }, [theme])
+
+  return <AppRouter />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <LanguageProvider>
-        <AppRouter />
+        <RootWrapper />
       </LanguageProvider>
     </ThemeProvider>
   </StrictMode>

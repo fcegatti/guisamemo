ee2c3d9195dc824f72ed786f5e989ea8775afc9f
fcegatti <fcegatti@yahoo.es>
2025-05-07 23:38:47 -0300

    chore: clean up manual manifest and service worker logic

--- src/main.jsx
+++ src/main.jsx
@@ -24,36 +24,3 @@ createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </StrictMode>
 )
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js')
-      .then((registration) => {
-        if (registration.waiting && navigator.serviceWorker.controller) {
-          window.dispatchEvent(new CustomEvent('sw-update'))
-        }
-
-        registration.addEventListener('updatefound', () => {
-          const newWorker = registration.installing
-          newWorker?.addEventListener('statechange', () => {
-            if (
-              newWorker.state === 'installed' &&
-              navigator.serviceWorker.controller &&
-              import.meta.env.MODE !== 'development'
-            ) {
-              window.dispatchEvent(new CustomEvent('sw-update'))
-            }
-
-            if (
-              newWorker.state === 'activated' &&
-              import.meta.env.MODE !== 'development'
-            ) {
-              window.dispatchEvent(new CustomEvent('sw-update'))
-            }
-          })
-        })
-      })
-      .catch(() => {})
-  })
-}

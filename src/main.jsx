ecb495d2b6d13775a3a0277586e5f3aeb0dd414a
fcegatti <fcegatti@yahoo.es>
2025-05-07 23:48:22 -0300

    feat: register service worker using vite-plugin-pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from 'virtual:pwa-register'
 import AppRouter from './AppRouter.jsx'
 import { LanguageProvider } from '@context/LanguageContext'
 import { ThemeProvider, useTheme } from '@context/ThemeContext'
 import '@styles/index.css'
+
+registerSW({
+  onNeedRefresh () {
+    window.dispatchEvent(new CustomEvent('sw-update'))
+  },
+  onOfflineReady () {
+    console.log('[PWA] App ready to work offline')
+  }
+})
 
 function RootWrapper () {
   const { theme } = useTheme()

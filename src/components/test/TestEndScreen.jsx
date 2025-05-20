ccd515dec28f973bc3b546b40e7e15110d08a010
fcegatti <fcegatti@yahoo.es>
2025-05-20 01:35:41 -0300

    test: restrict localStorage override to /test-end route only

--- src/components/test/TestEndScreen.jsx
+++ src/components/test/TestEndScreen.jsx
@@ -1,5 +1,6 @@
 /* global localStorage */
 
+import { useEffect } from 'react'
 import EndScreen from '../game/EndScreen'
 import { GameProvider } from '@context/GameContext'
 import { LanguageProvider } from '@context/LanguageContext'
@@ -13,13 +14,23 @@ const mockPlayers = [
     score: 40,
     avatar: 'avatar-10.webp',
     turns: 18
+  },
+  {
+    id: 'player-2',
+    name: 'Maradonga',
+    score: 40,
+    avatar: 'avatar-10.webp',
+    turns: 18
   }
 ]
 
-localStorage.setItem('theme', 'dark')
-localStorage.setItem('lang', 'gl')
-
 export default function TestEndScreen () {
+  useEffect(() => {
+    if (window.location.pathname === '/test-end') {
+      localStorage.setItem('theme', 'dark')
+      localStorage.setItem('lang', 'gl')
+    }
+  }, [])
   return (
     <LanguageProvider>
       <ThemeProvider>

07fdd3ffadaf9d35041c1b51e6cc3a7dc6a86540
tr4p0 <fcegatti@yahoo.es>
2025-06-30 13:28:59 -0300

    feat: improve touch targets for theme and language selectors to meet WCAG 44x44px standard

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -74,7 +74,7 @@ function StartScreen () {
           decoding='async'
           loading='eager'
         />
-        <nav aria-label={t.nav.selectors}>
+        <nav aria-label={t.nav.selectors} className='startscreen__nav'>
           <ThemeSelector />
           <LanguageSelector />
         </nav>

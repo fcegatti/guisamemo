c8752e943044abf1df42b24f899684fe5e000985
tr4p0 <fcegatti@yahoo.es>
2025-07-07 12:42:06 -0300

    refactor: move LanguageRedirector to interface subdirectory

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import LanguageRedirector from '@components/LanguageRedirector'
+import LanguageRedirector from '@components/interface/LanguageRedirector'
 import NotFound from '@components/interface/NotFound'
 import TestEndScreen from '@components/test/TestEndScreen'
 
